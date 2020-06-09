434ab5b53fa0996f51acfe431aa800f909262a21
Vbj <ijalab1808@gmail.com>
2020-06-09 18:12:34 +0530

    added styled global component

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Player from './Player';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import GlobalStyle from '../styles/Global';
 
 const App = () => (
     <BrowserRouter>
+    <>
         <Switch>
             <Route exact path = "/" component={Player} />
             //using the same component in below route becoz its not necessary to change the view
             <Route exact path = "/:curVideo" component={Player} /> 
         </Switch>
+        <GlobalStyle />
+    </>
     </BrowserRouter>
 )
 
