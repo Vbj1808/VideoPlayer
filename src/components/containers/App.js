eeca681e0ad5fd22593b282ab934159b32d6d6de
Vbj <ijalab1808@gmail.com>
2020-06-09 17:04:44 +0530

    added routes

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Player from './Player';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 const App = () => (
-    <Player />
+    <BrowserRouter>
+        <Switch>
+            <Route exact path = "/" component={Player} />
+            //using the same component in below route becoz its not necessary to change the view
+            <Route exact path = "/:curVideo" component={Player} /> 
+        </Switch>
+    </BrowserRouter>
 )
 
 export default App;
